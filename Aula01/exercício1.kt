315c3114d4c7056129fe6db7f51172d8c299bc63
luscasmbs <lucasmiguelbsilva@gmail.com>
2026-04-08 21:07:29 -0300

    Create exercício1.kt

---
+++ Aula01/exercício1.kt
@@ -0,0 +1,11 @@
+fun main() {
+println("Insira um valor qualquer");
+var numero = readln().toInt();
+    if (numero > 0){
+        println("Esse número é positivo")
+    } else if (numero == 0){
+        println("O número é 0")
+    } else {
+        println("Esse número é negativo")
+    }
+}
